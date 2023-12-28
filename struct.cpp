949dcea9022b163ecbb0ff521f6e54e0cfd7dd9a
Abdul Samad <86289220+junejosamad@users.noreply.github.com>
2023-12-28 18:52:25 +0500

    Update struct.cpp

--- struct.cpp
+++ struct.cpp
@@ -46,39 +46,18 @@ struct lvl2Enemy {
 };
 
 struct lvl3Enemy {
-	int medusa = 50;
-	int centaur = 60;
-	int sphnix = 65;
-	int roc = 75;
+	int medusa = 65;
+	int centaur = 70;
+	int sphnix = 80;
+	int roc = 95;
+};
+
+struct lvl4Enemy {
+	int naga = 70;
+	int tBird = 90;
+	int tengue = 100;
+	int valkyrie = 120;
 };
 
 
-//
-//struct Weapon {
-//    string name;
-//    string category;
-//    int damage;
-//    double fireRate;
-//    int magazineSize;
-//    Weapon(const string& n, const string& cat, int dmg, double rate, int magSize) : name(n), category(cat), damage(dmg), fireRate(rate), magazineSize(magSize) {}
-//};
-//int main() {
-//    Weapon m416(" M416 ", " Assault Rifle", 41, 0.086, 30);
-//    Weapon akm(" AKM ", " Assault Rifle", 49, 0.1, 30);
-//    Weapon kar98k(" Kar98k ", " Sniper Rifle", 75, 1.9, 5);
-//    Weapon pan(" Pan ", " Melee ", 80, 0, 0);
-//    Weapon m762(" M762 ", " Assault Rifle ", 47, 0.086, 30);
-//    Weapon awm(" AWM ", " Sniper Rifle ", 105, 1.85, 5);
-//    Weapon s12k(" S12K ", " Shotgun ", 22, 0.25, 5);
-//    Weapon ump45(" Ump45 ", " Smg ", 26, 0.045, 25);
-//
-//    vector<Weapon> pubgWeapons;
-//    pubgWeapons.push_back(m416);
-//    pubgWeapons.push_back(akm);
-//    pubgWeapons.push_back(kar98k);
-//    pubgWeapons.push_back(pan);
-//    pubgWeapons.push_back(m762);
-//    pubgWeapons.push_back(awm);
-//    pubgWeapons.push_back(s12k);
-//    pubgWeapons.push_back(ump45);
-//}
+
