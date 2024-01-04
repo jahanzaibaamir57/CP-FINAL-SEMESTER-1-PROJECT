7c39ca067a7d6230ea4965cefce041ad6fa11324
Abdul Samad <86289220+junejosamad@users.noreply.github.com>
2024-01-04 23:06:36 +0500

    Update struct.cpp

--- struct.cpp
+++ struct.cpp
@@ -1,62 +1,51 @@
 #include<string>
-
 using namespace std;
-
 //It stores Character Information
 struct characterInfo {
-	string characterName = "Player";
+	string characterName = "Default Player";
 	string characterRank = "Warrior";
 	int characterHealth = 100;
-	int characterExp = 0;//Level points
-	int exp = 0;//Global Exp points
+	int characterExp = 0;//Level exp points which will be reset in every level
+	int exp = 0;//total Exp points
 };
 
-
-//It stores quests
-struct quests {
-	string questsGold[10] = { "Gold +10 gram","Gold +12 gram",
-	"Gold +14 gram", "Gold +16 gram", "Gold +17 gram", "Gold +18 gram",
-		"Gold +19 gram", "Gold +20 gram", "Gold +23 gram", "Gold +25 gram"
-	};
-
-
-};
 
 struct inventorySys {
-	string weapons[5] = { "Sword", "Golden Sword", "Arrow", "Gun", "One Shot Gun"};
+	string weapons[5] = {};//Weapons that are unlocked
+	string lockedWeapons[5] = { "Sword", "Golden Sword", "Arrow", "Gun", "One Shot Gun" };//Weapons which are locked
 
 };
 
+
+//Health of Level 1 Enemies
 struct lvl1Enemy {
-	int baby = 40;
-	int adult = 50;
-	int giant = 70;
+	int baby = 40;			//Enemy 1 baby dragon whose health is 40
+	int adult = 50;			//Enemy 2 adult dragon whose health is 50
+	int giant = 70;			//Enemy 3 giant dragon whose healht is 70
 };
 
+
+//Level
 struct levels {
-	int lvl = 1;
+	short int lvl = 1;
 };
 
+
+//Health of level 2 Enemies
 struct lvl2Enemy {
-	//Phoneix = 75
-	int bennu = 50;
-	int fenghuang = 60;
-	int fireBird = 65;
-	int huma = 75;
+	int bennu = 50;			//Enemy 1 bennu phoneix whose health is 50 points 
+	int fenghuang = 60;		//Enemy 2 fenghuang phoneix whose health is 60 points
+	int fireBird = 65;		//Enemy 3 fireBird phoneix whose health is 65 points
+	int huma = 75;			//Enemy 4 huma phoneix whose health is 75 points
 };
 
+
+//Health of level 3 Enemies
 struct lvl3Enemy {
-	int medusa = 65;
-	int centaur = 70;
-	int sphnix = 80;
-	int roc = 95;
-};
-
-struct lvl4Enemy {
-	int naga = 70;
-	int tBird = 90;
-	int tengue = 100;
-	int valkyrie = 120;
+	int naga = 70;			//Enemy 1 naga whose health is 70 points
+	int tBird = 90;			//Enemy 2 tBird whose health is 90 points
+	int tengue = 100;		//Enemy 3 tengue whose health is 100 points
+	int valkyrie = 120;		//Enemy 4 valkyrie whose health is 120 points
 };
 
 
